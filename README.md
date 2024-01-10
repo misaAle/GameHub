829a1c3787849328fde7514ec9ba6dc68e988106
misaAle <misaelcasarez93@gmail.com>
2024-01-09 19:33:19 -0800

    Created simple express function supplied to http server

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # GameHub
 Future hub for simple multiplayer games
+
+
+Pre-requisites:
+- Have node installed (v20.10.0 works)
+
+Once repo has been cloned locally, run ``npm install``
+- This will install all dependencies required
+
+To start the server, run ''npm run start''
+- This will execute the "start" script defined in *package.json*, ``nodemon server.js``
+
+Navigate to your browser and open http://localhost:3000, and it should start the default tictactoe game and it should log in the console socket connections to the server
---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+// For now, client is served as static but it should be a react app in the future
+app.use(express.static('client'));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
